35ab597f2bda2318c77ba2633500ecebe0400357
Danijel Dragicevic <077226@canda.com>
2023-03-09 01:13:16 +0100

    Implement displaying code snippet by id, in the HTML form

--- src/main/java/com/devdan/platform/controllers/ICodeHtmlController.java
+++ src/main/java/com/devdan/platform/controllers/ICodeHtmlController.java
@@ -2,10 +2,14 @@ package com.devdan.platform.controllers;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/code")
 public interface ICodeHtmlController {
     @GetMapping("/new")
     String createCodeSnippet(Model model);
+    
+    @GetMapping("/{id}")
+    String getCodeSnippetById(Model model, @PathVariable("id") String id);
 }
